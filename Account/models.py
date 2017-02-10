dfd39c9b0967d16831c9d7b10578b0c1e721b162
yehiamlabib <yehiamlabib@gmail.com>
2017-02-10 15:33:11 -0500

    First commit

---
+++ Account/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db import models
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
+# Create your models here.
+class client(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    logo = models.CharField(max_length=90)
+    name = models.CharField(max_length=20)
+    phone_num = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    description = models.TextField( null=True)
+    def __str__(self):
+        return self.name
