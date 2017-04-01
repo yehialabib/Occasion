8759161bf6dcec1c83880df12469e48353bcac9f
yehialabib <yehiamlabib@gmail.com>
2017-04-01 19:17:57 +0200

    Edit Offers

--- Offer/functions.py
+++ Offer/functions.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from Account.models import follow
 from django.db.models import Sum
 
-def get_total_views(client):
-  total = offer.objects.filter(client=client).aggregate(Sum('views'))
+def get_total_views(client,date):
+  total = offer.objects.filter(client=client,date=timestamp).aggregate(Sum('views'))
   return total
 
-def get_total_clicks(client):
-  total = offer.objects.filter(client=client).aggregate(Sum('likes'))
+def get_total_clicks(client,date):
+  total = offer.objects.filter(client=client,date=timestamp).aggregate(Sum('likes'))
   return total
   
 def get_all_offers(client):
@@ -26,9 +26,16 @@ def add_offer(client, title , photo):
     
 def delete(ofer):
     offer.objects.filter(id=ofer).delete()
+
+def edit_offer(offer1, title, description): 
+    off=offer.objects.filter(id=offer1).first()
+    off.title=title
+    off.description=description
+    off.save()
     
 # Testings
 #print(get_all_offers(client.objects.filter(name='Fridays').first()))
 # print(serializers.serialize("json", get_all_user_offers(User.objects.filter(username="youssef").first())))
 #add_offer(client.objects.filter(name='Nike').first(), "New Mercurial Boots","Your dream football shoes are out now!","photo")
 #delete(offer.objects.filter(title="1234").first())
+edit_offer(18, "New Title","New Description")
